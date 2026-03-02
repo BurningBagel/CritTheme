56f41a503b6a4740d04907e3b33882f2c2450fd3
Luiz <luiz.duarte11@yahoo.com.br>
2026-03-02 14:29:58 -0300

    cleanup

--- scripts/hooks/init.js
+++ scripts/hooks/init.js
@@ -5,11 +5,11 @@ export const Init = {
         Hooks.once("init", () => {
             game.settings.register(consts.MODULE_NAME, 'play-time', {
                 name: 'Crit Song duration (seconds)',
-                scope: 'world',     // "world" = sync to db, "client" = local storage
-                config: true,       // false if you dont want it to show in module config
-                type: Number,       // You want the primitive class, e.g. Number, not the name of the class as a string
+                scope: 'world',     
+                config: true,       
+                type: Number,      
                 default: 5,
-                requiresReload: false, // true if you want to prompt the user to reload
+                requiresReload: false, 
                 filePicker: "any"
             });
 
